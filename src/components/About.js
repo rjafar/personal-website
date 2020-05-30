c6686589d9035917a9aa44935ed4b1a50d8a124d
Revan Jafar <revanjafar@gmail.com>
2020-05-30 11:50:44 -0700

    Finished welcome banner

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import avatar from '../avatar.png';
+import '../css/About.css';
+
+function About() {
+    return(
+        <section className="filler">
+            <div className="about-outer">
+                <img src="/favicon.ico" alt=""/>
+            </div>
+        </section>
+    );
+}
+
+export default About;
